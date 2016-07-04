5aa7d42b9b19d09683ba5cb5482433d7a2279957
Colin Seymour <lildood@gmail.com>
2016-07-04 11:25:00 +0100

    Update for latest fetch update

--- pretty-fetch.js
+++ pretty-fetch.js
@@ -1,3 +1,4 @@
+
 function changeSourceAll(original, newimg) {
     var images = document.getElementsByTagName('img');
     for (var i = 0; i < images.length; i++) {
@@ -6,14 +7,60 @@ function changeSourceAll(original, newimg) {
         }
     }
 }
-// This is far from efficient.
-changeSourceAll("http://www.fetcheveryone.com/images/feeling_2.gif", "http://twemoji.maxcdn.com/16x16/1f60d.png");
-changeSourceAll("http://www.fetcheveryone.com/images/feeling_0.gif", "http://twemoji.maxcdn.com/16x16/1f610.png");
-changeSourceAll("http://www.fetcheveryone.com/images/feeling_-1.gif", "http://twemoji.maxcdn.com/16x16/1f626.png");
-changeSourceAll("http://www.fetcheveryone.com/images/feeling_1.gif", "http://twemoji.maxcdn.com/16x16/1f600.png");
-changeSourceAll("http://www.fetcheveryone.com/images/feeling_-2.gif", "http://twemoji.maxcdn.com/16x16/1f622.png");
-//http://www.fetcheveryone.com/images/feeling_2.gif => http://twemoji.maxcdn.com/16x16/1f60d.png
-//http://www.fetcheveryone.com/images/feeling_0.gif => http://twemoji.maxcdn.com/16x16/1f610.png
-//http://www.fetcheveryone.com/images/feeling_-1.gif => http://twemoji.maxcdn.com/16x16/1f626.png
-//http://www.fetcheveryone.com/images/feeling_1.gif => http://twemoji.maxcdn.com/16x16/1f600.png
-//http://www.fetcheveryone.com/images/feeling_-2.gif => http://twemoji.maxcdn.com/16x16/1f622.png
+
+(function(){
+
+    var loc = document.location.href;
+
+    /* Training log specific javascript actions */
+    if(/fetcheveryone\.com\/training\.php/.test(loc)){
+
+        /* */
+        document.addEventListener('DOMSubtreeModified', injectCSS, false);
+
+        function injectCSS(){
+            if(document.head){
+                document.removeEventListener('DOMSubtreeModified', injectCSS, false);
+                var link = document.createElement("link");
+                link.href = chrome.extension.getURL("font-awesome.min.css");
+                link.type = "text/css";
+                link.rel = "stylesheet";
+
+                document.head.appendChild(link);
+            }
+        }
+
+        var images = document.getElementsByTagName('img');
+
+
+        for (var i = 0; i < images.length; i++) {
+          var trash = document.createElement('i');
+          trash.className = 'fa fa-trash';
+          if (images[i].src == "http://www.fetcheveryone.com/images/icons/delete.png") {
+            images[i].parentNode.replaceChild(trash, images[i]);
+          }
+        }
+
+        // Change the images for emojis. This is far from efficient.
+        changeSourceAll("http://www.fetcheveryone.com/images/feeling_2.gif", "http://twemoji.maxcdn.com/2/svg/1f60d.svg");
+        changeSourceAll("http://www.fetcheveryone.com/images/feeling_0.gif", "http://twemoji.maxcdn.com/2/svg/1f610.svg");
+        changeSourceAll("http://www.fetcheveryone.com/images/feeling_-1.gif", "http://twemoji.maxcdn.com/2/svg/1f626.svg");
+        changeSourceAll("http://www.fetcheveryone.com/images/feeling_1.gif", "http://twemoji.maxcdn.com/2/svg/1f600.svg");
+        changeSourceAll("http://www.fetcheveryone.com/images/feeling_-2.gif", "http://twemoji.maxcdn.com/2/svg/1f622.svg");
+        //http://www.fetcheveryone.com/images/feeling_2.gif => http://twemoji.maxcdn.com/16x16/1f60d.png
+        //http://www.fetcheveryone.com/images/feeling_0.gif => http://twemoji.maxcdn.com/16x16/1f610.png
+        //http://www.fetcheveryone.com/images/feeling_-1.gif => http://twemoji.maxcdn.com/16x16/1f626.png
+        //http://www.fetcheveryone.com/images/feeling_1.gif => http://twemoji.maxcdn.com/16x16/1f600.png
+        //http://www.fetcheveryone.com/images/feeling_-2.gif => http://twemoji.maxcdn.com/16x16/1f622.png
+    }
+
+    /* Editing training page - change columns to 50% width */
+    if(/fetcheveryone\.com\/training-edit\.php/.test(loc)){
+      document.getElementsByClassName("fespan_2_of_3")[0].className = "fecol fespan_1_of_2";
+      document.getElementsByClassName("fespan_1_of_3")[0].className = "fecol fespan_1_of_2";
+
+      /* Add my own class to various boxes */
+      document.getElementsByClassName("infobox_style3")[0].className += " col-box0";
+    }
+
+})()
